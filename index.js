46417c316a012c159e5c02928cf43a6291689779
Jason Tavera <taverajason7@gmail.com>
2023-05-01 23:04:09 -0500

    added prompt and SVG Class

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+const { Triangle, Circle, Square } = require("./lib/shapes");
+const inquirer = require("inquirer");
+
+const startPrompt = async () => {
+  const startPrompt = await inquirer.prompt([
+    {
+      type: "list",
+      name: "shape",
+      message: "What shape would you like to use?",
+    },
+    {
+      type: "input",
+      name: "text",
+      message: "What text would you like to display on your shape?",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What text color would you like to use?",
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "What shape color would you like to use?",
+    },
+  ]);
+};
+
+let shape;
+switch (initialPrompt.shape) {
+  case "circle":
+    shape = new Circle();
+    break;
+  case "square":
+    shape = new Square();
+    break;
+  case "triangle":
+    shape = new Triangle();
+    break;
+  default:
+    throw Error("Error creating the SVG Shape");
+}
 const fs = require("fs");
 let SVG = require("./lib/svg");
 
